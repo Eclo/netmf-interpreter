957afedbdc3e0495ab5439c134ef21c0ece3f679
Jose Simoes <jose.simoes@eclo.pt>
2016-03-11 15:42:18 +0000

    Add CMSIS includes for STM32F4 series

--- DeviceCode/include/tinyhal.h
+++ DeviceCode/include/tinyhal.h
@@ -1540,4 +1540,20 @@ extern bool g_fDoNotUninitializeDebuggerPort;
 
 //--//
 
+// HAL includes for CMSIS vendor implementations
+
+// STM32F4 series
+#if defined STM32F405xx|| defined STM32F415xx|| defined STM32F407xx|| defined STM32F417xx|| \
+    defined STM32F427xx|| defined STM32F437xx|| defined STM32F429xx|| defined STM32F439xx|| \
+    defined STM32F401xC|| defined STM32F401xE|| defined STM32F410Tx|| defined STM32F410Cx|| \
+    defined STM32F410Rx|| defined STM32F411xE|| defined STM32F446xx|| defined STM32F469xx|| \
+    defined STM32F479xx
+
+#include "stm32f4xx_hal.h"
+
+#endif
+
+
+//--//
+
 #endif  // _TINYHAL_H_
