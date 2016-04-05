094fcc23e3e937e691fb71955f4ab7c766ef55ea
sjmneves <sergio.neves@eclo.pt>
2016-04-05 17:33:49 +0100

    Complete CMSIS DA driver

--- DeviceCode/Targets/Native/STM32F4xx/DA/DA_functions.cpp
+++ DeviceCode/Targets/Native/STM32F4xx/DA/DA_functions.cpp
@@ -16,8 +16,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #define DA_CHANNELS 2       // number of channels
-#define DA_FIRST_PIN 4      // channel 0 pin (A4)
 
+#if (!defined(USE_DAC_CHANNEL_1) && !defined(USE_DAC_CHANNEL_2) && !defined(USE_BOTH_DAC_CHANNEL)) 
+#error "No DAC defines! Please go to platform_selector.h (in solution folder) and uncomment one and only one DAC define"
+#endif
+#if ((defined(USE_DAC_CHANNEL_2) && !defined(DAC_CHANNEL_2)) || (defined(USE_BOTH_DAC_CHANNEL) && !defined(DAC_CHANNEL_2))) 
+#error "DAC channel 2 doesn't exist! Please go to platform_selector.h (in solution folder) and comment defines USE_DAC_CHANNEL_2 and USE_BOTH_DAC_CHANNEL"
+#endif
+#if ((defined(USE_DAC_CHANNEL_1) && defined(USE_DAC_CHANNEL_2)) || (defined(USE_DAC_CHANNEL_1) && defined(USE_BOTH_DAC_CHANNEL)) || (defined(USE_DAC_CHANNEL_2) && defined(USE_BOTH_DAC_CHANNEL))) 
+#error "There can be only one define! Please go to platform_selector.h (in solution folder) and comment DAC defines less one"
+#endif
 //--//
 
 DAC_HandleTypeDef DacHandle;
@@ -41,11 +49,28 @@ void HAL_DAC_MspInit(DAC_HandleTypeDef* hdac)
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
     /*##-2- Configure peripheral GPIO ##########################################*/ 
-    /* DAC Channel1 GPIO pin configuration */
-    GPIO_InitStruct.Pin = GPIO_PIN_4;
+    /* DAC Channels GPIO pin configuration */
+#ifdef USE_BOTH_DAC_CHANNEL
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
+    // channel 1 in pin PA4
+    GPIO_InitStruct.Pin = GPIO_PIN_4;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    // channel 2 in pin PA5
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#else
+    #if USE_DAC_CHANNEL_2
+        // channel 2 in pin PA5
+        GPIO_InitStruct.Pin = GPIO_PIN_5;
+    #else
+        // channel 1 in pin PA4
+        GPIO_InitStruct.Pin = GPIO_PIN_4;
+    #endif
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#endif
 }
   
 /**
@@ -56,15 +81,20 @@ void HAL_DAC_MspInit(DAC_HandleTypeDef* hdac)
   */
 void HAL_DAC_MspDeInit(DAC_HandleTypeDef* hdac)
 {
-  static DMA_HandleTypeDef  hdma_dac1;
-  
-  /*##-1- Reset peripherals ##################################################*/
-  __HAL_RCC_DAC_FORCE_RESET();
-  __HAL_RCC_DAC_RELEASE_RESET();
+    /*##-1- Reset peripherals ##################################################*/
+    __HAL_RCC_DAC_FORCE_RESET();
+    __HAL_RCC_DAC_RELEASE_RESET();
 
-  /*##-2- Disable peripherals and GPIO Clocks ################################*/
-  /* De-initialize the DAC Channel1 GPIO pin */
-  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+    /*##-2- Disable peripherals and GPIO Clocks ################################*/
+    /* De-initialize the DAC Channels GPIO pin */
+#ifdef USE_BOTH_DAC_CHANNEL
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5);
+#elif USE_DAC_CHANNEL_2
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5);
+#else
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+#endif
 }
 
 BOOL DA_Initialize(DA_CHANNEL channel, INT32 precisionInBits)
@@ -73,37 +103,32 @@ BOOL DA_Initialize(DA_CHANNEL channel, INT32 precisionInBits)
     
     if (precisionInBits != 12) return FALSE;
 
-    // FIXME *##-1- Configure the DAC peripheral #######################################*/
+    // Configure the DAC peripheral
     DacHandle.Instance = DAC;
-    /*##-1- Initialize the DAC peripheral ######################################*/
+    // Initialize the DAC peripheral
     if(HAL_DAC_Init(&DacHandle) != HAL_OK)
     {
-        /* Initialization Error */
+        // Initialization Error
         return FALSE;
     }
 
-    /*##-2- Configure DAC channel1 #############################################*/
+    // Configure DAC channel
     sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
     sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_DISABLE;
 
+#ifdef USE_BOTH_DAC_CHANNEL
     if (channel) 
     {
         // enable channel 2
         if(HAL_DAC_ConfigChannel(&DacHandle, &sConfig, DAC_CHANNEL_2) != HAL_OK)
         {
-            /* Channel configuration Error */
+            // Channel configuration Error
             return FALSE;
         }
-        /*##-3- Set DAC Channel1 DHR register ######################################*/
-        if (HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_2, DAC_ALIGN_12B_R, 0xFF) != HAL_OK)
-        {
-            /* Setting value Error */
-            return FALSE;
-        }
-        /*##-4- Enable DAC Channel ################################################*/
+        // Enable DAC Channel
         if (HAL_DAC_Start(&DacHandle, DAC_CHANNEL_2) != HAL_OK)
         {
-            /* Start Error */
+            // Start Error
             return FALSE;
         }
     } 
@@ -112,87 +137,109 @@ BOOL DA_Initialize(DA_CHANNEL channel, INT32 precisionInBits)
         // enable channel 1
         if(HAL_DAC_ConfigChannel(&DacHandle, &sConfig, DAC_CHANNEL_1) != HAL_OK)
         {
-            /* Channel configuration Error */
+            // Channel configuration Error
             return FALSE;
         }
-        /*##-3- Set DAC Channel1 DHR register ######################################*/
-        if (HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 0xFF) != HAL_OK)
-        {
-            /* Setting value Error */
-            return FALSE;
-        }
-        /*##-4- Enable DAC Channel ################################################*/
+        // Enable DAC Channel
         if (HAL_DAC_Start(&DacHandle, DAC_CHANNEL_1) != HAL_OK)
         {
-            /* Start Error */
+            // Start Error
             return FALSE;
         }
     }
+#else
+    #if USE_DAC_CHANNEL_2
+        // enable channel 2
+        if(HAL_DAC_ConfigChannel(&DacHandle, &sConfig, DAC_CHANNEL_2) != HAL_OK)
+        {
+            // Channel configuration Error
+            return FALSE;
+        }
+        // Enable DAC Channel
+        if (HAL_DAC_Start(&DacHandle, DAC_CHANNEL_2) != HAL_OK)
+        {
+            // Start Error
+            return FALSE;
+        }
+    #else
+        // enable channel 1
+        if(HAL_DAC_ConfigChannel(&DacHandle, &sConfig, DAC_CHANNEL_1) != HAL_OK)
+        {
+            // Channel configuration Error
+            return FALSE;
+        }
+        // Enable DAC Channel
+        if (HAL_DAC_Start(&DacHandle, DAC_CHANNEL_1) != HAL_OK)
+        {
+            // Start Error
+            return FALSE;
+        }
+    #endif
+#endif
 
-    // // enable DA clock
-    // RCC->APB1ENR |= RCC_APB1ENR_DACEN;
-    // // set pin as analog
-    // CPU_GPIO_DisablePin(DA_FIRST_PIN + channel, RESISTOR_DISABLED, 0, GPIO_ALT_MODE_1);
-    // if (channel) {
-    //     DAC->CR |= DAC_CR_EN2; // enable channel 2
-    // } else {
-    //     DAC->CR |= DAC_CR_EN1; // enable channel 1
-    // }
     return TRUE;
 }
 
 void DA_Uninitialize(DA_CHANNEL channel)
 {
-    // FIXME
     HAL_DAC_DeInit(&DacHandle);
-    
-    // if (channel) {
-    //     DAC->CR &= ~DAC_CR_EN2; // disable channel 2
-    // } else {
-    //     DAC->CR &= ~DAC_CR_EN1; // disable channel 1
-    // }
-    // // free pin
-    // CPU_GPIO_DisablePin(DA_FIRST_PIN + channel, RESISTOR_DISABLED, 0, GPIO_ALT_PRIMARY);
-    // if ((DAC->CR & (DAC_CR_EN1 | DAC_CR_EN2)) == 0) { // all channels off
-    //     // disable DA clock
-    //     RCC->APB1ENR &= ~RCC_APB1ENR_DACEN;
-    // }
 }
 
 // level is a 12 bit value
 void DA_Write(DA_CHANNEL channel, INT32 level)
 {
+    // set value
+#ifdef USE_BOTH_DAC_CHANNEL
     if (channel) 
     {
-        DAC->DHR12R2 = level;
+        HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_2, DAC_ALIGN_12B_R, level);
     } 
     else 
     {
-        DAC->DHR12R1 = level;
+        HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_1, DAC_ALIGN_12B_R, level);
     }
+#else
+    #if USE_DAC_CHANNEL_2
+    HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_2, DAC_ALIGN_12B_R, level);
+    #else
+    HAL_DAC_SetValue(&DacHandle, DAC_CHANNEL_1, DAC_ALIGN_12B_R, level);
+    #endif
+#endif
 }
 
 UINT32 DA_DAChannels()
 {
-    return DA_CHANNELS;
+#ifdef DAC_CHANNEL_2
+    return 2;
+#else
+    return 1;
+#endif
 }
 
 GPIO_PIN DA_GetPinForChannel(DA_CHANNEL channel)
 {
-    if ((UINT32)channel >= DA_CHANNELS) 
+#ifdef USE_BOTH_DAC_CHANNEL
+    if (channel) 
     {
-        return GPIO_PIN_NONE;
+        // DAC_CHANNEL_2 on PA5
+        return GPIO_PIN_5;
+    } 
+    else 
+    {
+        // DAC_CHANNEL_2 on PA4
+        return GPIO_PIN_4;
     }
-    return DA_FIRST_PIN + channel;
+#elif USE_DAC_CHANNEL_2
+    return GPIO_PIN_5;
+#else
+    return GPIO_PIN_4;
+#endif 
 }
 
 BOOL DA_GetAvailablePrecisionsForChannel(DA_CHANNEL channel, INT32* precisions, UINT32& size)
 {
-    size = 0;
-    if (precisions == NULL || (UINT32)channel >= DA_CHANNELS) 
-    {
-        return FALSE;
-    }
+    // FIXME - complete code with hardware availabel precisions
+    // Considering all channels with same precision
     precisions[0] = 12;
     size = 1;
     return TRUE;
