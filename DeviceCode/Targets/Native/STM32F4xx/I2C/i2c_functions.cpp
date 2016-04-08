c70699cf6f2ff1b1303642b5214ce7d9987e3d1e
sjmneves <sergio.neves@eclo.pt>
2016-04-08 13:05:52 +0100

    Complete CMSIS I2C driver

--- DeviceCode/Targets/Native/STM32F4xx/I2C/i2c_functions.cpp
+++ DeviceCode/Targets/Native/STM32F4xx/I2C/i2c_functions.cpp
@@ -40,7 +40,7 @@
 //         #define I2C_SDA_PIN  41 // PC9
 //     #endif
 // #else // use I2C1 by default
-        #define I2Cx                   I2C1 
+    #define I2Cx                   I2C1 
     #define I2Cx_EV_IRQn           I2C1_EV_IRQn
     #define I2Cx_ER_IRQn           I2C1_ER_IRQn
     #define RCC_APB1ENR_I2CxEN     RCC_APB1ENR_I2C1EN
@@ -53,8 +53,19 @@
     #endif
 // #endif
 
+/* I2C handler declaration */
+I2C_HandleTypeDef I2cHandle;
+
 static I2C_HAL_XACTION* currentI2CXAction;
 static I2C_HAL_XACTION_UNIT* currentI2CUnit;
+
+// prototypes
+void Start_Master_Transmit(I2C_HAL_XACTION_UNIT* unit, uint16_t I2CAddress);
+void Start_Master_Receive(I2C_HAL_XACTION_UNIT* unit, uint16_t I2CAddress);
+void I2C_Tx_Rx_Completed();
+
+// RX buffer pointer
+uint8_t * rxBuffer = NULL;
 
 /**
   * @brief  This function handles I2C event interrupt request.  
@@ -64,7 +75,7 @@ static I2C_HAL_XACTION_UNIT* currentI2CUnit;
   */
 void I2C1_EV_IRQHandler(void)
 {
-    HAL_I2C_EV_IRQHandler(& I2cHandle);
+    HAL_I2C_EV_IRQHandler(&I2cHandle);
 }
 
 /**
@@ -75,27 +86,7 @@ void I2C1_EV_IRQHandler(void)
   */
 void I2C1_ER_IRQHandler(void)
 {
-    HAL_I2C_ER_IRQHandler(& I2cHandle);
-}
-
-/**
-  * @brief  Master Tx Transfer completed callbacks.
-  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
-  *         the configuration information for I2C module
-  * @retval None
-  */
-void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
-{
-}
-
-/**
-  * @brief  Master Rx Transfer completed callbacks.
-  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
-  *         the configuration information for I2C module
-  * @retval None
-  */
-void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
-{
+    HAL_I2C_ER_IRQHandler(&I2cHandle);
 }
 
 /**
@@ -106,153 +97,131 @@ void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
   */
 void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
 {
+    // free rx buffer 
+    if(rxBuffer != NULL)
+    {
+        free(rxBuffer);
+        rxBuffer = NULL;
+    }
+    
+    I2C_HAL_XACTION* xAction = currentI2CXAction;
+    xAction->Signal(I2C_HAL_XACTION::c_Status_Aborted); // calls XActionStop()
+}
+
+/**
+  * @brief  Master Tx Transfer completed callbacks.
+  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *         the configuration information for I2C module
+  * @retval None
+  */
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    I2C_Tx_Rx_Completed();
+}
+
+/**
+  * @brief  Master Rx Transfer completed callbacks.
+  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *         the configuration information for I2C module
+  * @retval None
+  */
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    for(int i = 0; i < hi2c->XferSize; i++)
+    {
+         // save next data byte into queue
+        *(currentI2CUnit->m_dataQueue.Push()) = rxBuffer[i];
+    }
+    // adjust counters
+    currentI2CUnit->m_bytesTransferred = hi2c->XferSize;
+    currentI2CUnit->m_bytesToTransfer -= hi2c->XferSize;
+
+    // free rx buffer
+    free(rxBuffer);
+    rxBuffer = NULL;
+
+    I2C_Tx_Rx_Completed();
+}
+
+void I2C_Tx_Rx_Completed()
+{
+    I2C_HAL_XACTION* xAction = currentI2CXAction;
+    
+    // all received or all sent
+    if (!xAction->ProcessingLastUnit()) 
+    {
+        // get current unit 
+        currentI2CUnit = xAction->m_xActionUnits[xAction->m_current++];
+
+        // start next unit
+        // what is the direction of the current I2C unit?
+        if (currentI2CUnit->IsReadXActionUnit()) 
+        { 
+            // read transaction
+            // get address from xAction        
+            Start_Master_Receive(currentI2CUnit, xAction->m_address << 1);
+        }
+        else
+        {
+            // write transaction
+            // get address from xAction
+            Start_Master_Transmit(currentI2CUnit, xAction->m_address << 1);
+        }
+    } 
+    else 
+    {
+        xAction->Signal(I2C_HAL_XACTION::c_Status_Completed); // calls XActionStop()
+    }
 }
 
 void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c)
 {
-  GPIO_InitTypeDef  GPIO_InitStruct;
-  
-  /*##-1- Enable peripherals and GPIO Clocks #################################*/
-  /* Enable GPIO TX/RX clock */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  /* Enable I2C1 clock */
-  __HAL_RCC_I2C1_CLK_ENABLE(); 
-  
-  /*##-2- Configure peripheral GPIO ##########################################*/  
-  /* I2C TX GPIO pin configuration  */
-  GPIO_InitStruct.Pin       = I2C_SCL_PIN;
-  GPIO_InitStruct.Mode      = GPIO_MODE_AF_OD;
-  GPIO_InitStruct.Pull      = GPIO_PULLUP;
-  GPIO_InitStruct.Speed     = GPIO_SPEED_FAST;
-  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-  
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    
-  /* I2C RX GPIO pin configuration  */
-  GPIO_InitStruct.Pin = I2C_SDA_PIN;
-  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-    
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    
-  /*##-3- Configure the NVIC for I2C #########################################*/   
-  /* NVIC for I2C1 */
-  HAL_NVIC_SetPriority(I2C1_ER_IRQn, 0, 1);
-  HAL_NVIC_EnableIRQ(I2C1_ER_IRQn);
-  HAL_NVIC_SetPriority(I2C1_EV_IRQn, 0, 2);
-  HAL_NVIC_EnableIRQ(I2C1_EV_IRQn);
+    GPIO_InitTypeDef  GPIO_InitStruct;
+
+    /*##-1- Enable peripherals and GPIO Clocks #################################*/
+    /* Enable GPIO TX/RX clock */
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /* Enable I2C1 clock */
+    __HAL_RCC_I2C1_CLK_ENABLE(); 
+
+    /*##-2- Configure peripheral GPIO ##########################################*/  
+    /* I2C TX GPIO pin configuration  */
+    GPIO_InitStruct.Pin       = I2C_SCL_PIN;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull      = GPIO_PULLUP;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FAST;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* I2C RX GPIO pin configuration  */
+    GPIO_InitStruct.Pin = I2C_SDA_PIN;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*##-3- Configure the NVIC for I2C #########################################*/   
+    /* NVIC priority for I2C1 */
+    HAL_NVIC_SetPriority(I2C1_ER_IRQn, 0, 1);
+    HAL_NVIC_SetPriority(I2C1_EV_IRQn, 0, 2);
 }
 
 void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c)
 {
-  /*##-1- Reset peripherals ##################################################*/
-  __HAL_RCC_I2C1_FORCE_RESET();
-  __HAL_RCC_I2C1_RELEASE_RESET();
+    /*##-1- Reset peripherals ##################################################*/
+    __HAL_RCC_I2C1_FORCE_RESET();
+    __HAL_RCC_I2C1_RELEASE_RESET();
 
-  /*##-2- Disable peripherals and GPIO Clocks ################################*/
-  /* Configure I2C Tx as alternate function  */
-  HAL_GPIO_DeInit(GPIOB, I2C_SCL_PIN);
-  /* Configure I2C Rx as alternate function  */
-  HAL_GPIO_DeInit(GPIOB, I2C_SDA_PIN);
-  
-  /*##-3- Disable the NVIC for I2C ###########################################*/
-  HAL_NVIC_DisableIRQ(I2C1_ER_IRQn);
-  HAL_NVIC_DisableIRQ(I2C1_EV_IRQn);
-}
+    /*##-2- Disable peripherals and GPIO Clocks ################################*/
+    /* Configure I2C Tx as alternate function  */
+    HAL_GPIO_DeInit(GPIOB, I2C_SCL_PIN);
+    /* Configure I2C Rx as alternate function  */
+    HAL_GPIO_DeInit(GPIOB, I2C_SDA_PIN);
 
-void I2C_ER_Interrupt (void* param) // Error Interrupt Handler
-{
-    INTERRUPT_START
-    
-    // pre:
-    // I2C_SR1_BERR | I2C_SR1_ARLO | I2C_SR1_AF | I2C_SR1_TIMEOUT
-    
-    I2C_HAL_XACTION* xAction = currentI2CXAction;
-    
-    I2Cx->SR1 = 0; // reset errors
-    xAction->Signal(I2C_HAL_XACTION::c_Status_Aborted); // calls XActionStop()
-    
-    INTERRUPT_END
+    /*##-3- Disable the NVIC for I2C ###########################################*/
+    HAL_NVIC_DisableIRQ(I2C1_ER_IRQn);
+    HAL_NVIC_DisableIRQ(I2C1_EV_IRQn);
 }
-    
-void I2C_EV_Interrupt (void* param) // Event Interrupt Handler
-{
-    INTERRUPT_START
-    
-    // pre:
-    // I2C_SR1_SB | I2C_SR1_ADDR | I2C_SR1_BTF | I2C_CR2_ITBUFEN & (I2C_SR1_RXNE | I2C_SR1_TXE)
-    
-    I2C_HAL_XACTION* xAction = currentI2CXAction;
-    I2C_HAL_XACTION_UNIT* unit = currentI2CUnit;
-    
-    int todo = unit->m_bytesToTransfer;
-    int sr1 = I2Cx->SR1;  // read status register
-    int sr2 = I2Cx->SR2;  // clear ADDR bit
-    int cr1 = I2Cx->CR1;  // initial control register
-    
-    if (unit->IsReadXActionUnit()) { // read transaction
-        if (sr1 & I2C_SR1_SB) { // start bit
-            if (todo == 1) {
-                I2Cx->CR1 = (cr1 &= ~I2C_CR1_ACK); // last byte nack
-            } else if (todo == 2) {
-                I2Cx->CR1 = (cr1 |= I2C_CR1_POS); // prepare 2nd byte nack
-            }
-            UINT8 addr = xAction->m_address << 1; // address bits
-            I2Cx->DR = addr + 1; // send header byte with read bit;
-        } else {
-            if (sr1 & I2C_SR1_ADDR) { // address sent
-                if (todo == 1) {
-                    I2Cx->CR1 = (cr1 |= I2C_CR1_STOP); // send stop after single byte
-                } else if (todo == 2) {
-                    I2Cx->CR1 = (cr1 &= ~I2C_CR1_ACK); // last byte nack
-                }
-            } else {
-                while (sr1 & I2C_SR1_RXNE) { // data available
-                    if (todo == 2) { // 2 bytes remaining
-                        I2Cx->CR1 = (cr1 |= I2C_CR1_STOP); // stop after last byte
-                    } else if (todo == 3) { // 3 bytes remaining
-                        if (!(sr1 & I2C_SR1_BTF)) break; // assure 2 bytes are received
-                        I2Cx->CR1 = (cr1 &= ~I2C_CR1_ACK); // last byte nack
-                    }
-                    UINT8 data = I2Cx->DR; // read data
-                    *(unit->m_dataQueue.Push()) = data; // save data
-                    unit->m_bytesTransferred++;
-                    unit->m_bytesToTransfer = --todo; // update todo
-                    sr1 = I2Cx->SR1;  // update status register copy
-                }
-            }
-            if (todo == 1) {
-                I2Cx->CR2 |= I2C_CR2_ITBUFEN; // enable I2C_SR1_RXNE interrupt
-            }
-        }
-    } else { // write transaction
-        if (sr1 & I2C_SR1_SB) { // start bit
-            UINT8 addr = xAction->m_address << 1; // address bits
-            I2Cx->DR = addr; // send header byte with write bit;
-        } else {
-            while (todo && (sr1 & I2C_SR1_TXE)) {
-                I2Cx->DR = *(unit->m_dataQueue.Pop()); // next data byte;
-                unit->m_bytesTransferred++;
-                unit->m_bytesToTransfer = --todo; // update todo
-                sr1 = I2Cx->SR1;  // update status register copy
-            }
-            if (!(sr1 & I2C_SR1_BTF)) todo++; // last byte not yet sent
-        }
-        
-    }
-    
-    if (todo == 0) { // all received or all sent
-        if (!xAction->ProcessingLastUnit()) { // start next unit
-            I2Cx->CR2 &= ~I2C_CR2_ITBUFEN; // disable I2C_SR1_RXNE interrupt
-            currentI2CUnit = xAction->m_xActionUnits[ xAction->m_current++ ];
-            I2Cx->CR1 = I2C_CR1_PE | I2C_CR1_START | I2C_CR1_ACK; // send restart
-        } else {
-            xAction->Signal(I2C_HAL_XACTION::c_Status_Completed); // calls XActionStop()
-        }
-    }
-    
-    INTERRUPT_END
-}
-
 
 BOOL I2C_Internal_Initialize()
 {
@@ -312,45 +281,45 @@ void I2C_Internal_XActionStart( I2C_HAL_XACTION* xAction, bool repeatedStart )
     I2cHandle.Init.DutyCycle       = I2C_DUTYCYCLE_16_9;
     I2cHandle.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
     I2cHandle.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
-    I2cHandle.Init.OwnAddress1     = xAction->m_address;//I2C_ADDRESS;
+    I2cHandle.Init.OwnAddress1     = xAction->m_address << 1;//I2C_ADDRESS;
     I2cHandle.Init.OwnAddress2     = 0xFE;
 
     if(HAL_I2C_Init(&I2cHandle) != HAL_OK)
     {
         // Initialization Error
-        return FALSE;    
+        return;    
     }
-    
 
+    // set vars for latter use
     currentI2CXAction = xAction;
-    currentI2CUnit = xAction->m_xActionUnits[ xAction->m_current++ ];
-    
-    UINT32 ccr = xAction->m_clockRate + (xAction->m_clockRate2 << 8);
-    if (I2Cx->CCR != ccr) { // set clock rate and rise time
-        UINT32 trise;
-        if (ccr & I2C_CCR_FS) { // fast => 0.3ns rise time
-            trise = SYSTEM_APB1_CLOCK_HZ / (1000 * 3333) + 1; // PCLK1 / 3333kHz
-        } else { // slow => 1.0ns rise time
-            trise = SYSTEM_APB1_CLOCK_HZ / (1000 * 1000) + 1; // PCLK1 / 1000kHz
-        }
-        I2Cx->CR1 = 0; // disable peripheral
-        I2Cx->CCR = ccr;
-        I2Cx->TRISE = trise;
+    currentI2CUnit = xAction->m_xActionUnits[xAction->m_current++];
+
+    // enable interrupts
+    HAL_NVIC_EnableIRQ(I2C1_ER_IRQn);
+    HAL_NVIC_EnableIRQ(I2C1_EV_IRQn);
+
+    // what is the direction of the current I2C unit?
+    if (currentI2CUnit->IsReadXActionUnit()) 
+    { 
+        // read transaction
+        // get address from xAction        
+        Start_Master_Receive(currentI2CUnit, xAction->m_address << 1);
     }
-    
-    I2Cx->CR1 = I2C_CR1_PE; // enable and reset special flags
-    I2Cx->SR1 = 0; // reset error flags
-    I2Cx->CR2 |= I2C_CR2_ITEVTEN | I2C_CR2_ITERREN; // enable interrupts
-    I2Cx->CR1 = I2C_CR1_PE | I2C_CR1_START | I2C_CR1_ACK; // send start
+    else
+    {
+        // write transaction
+        // get address from xAction
+        Start_Master_Transmit(currentI2CUnit, xAction->m_address << 1);
+     }
 }
 
 void I2C_Internal_XActionStop()
 {
     NATIVE_PROFILE_HAL_PROCESSOR_I2C();
-    if (I2Cx->SR2 & I2C_SR2_BUSY && !(I2Cx->CR1 & I2C_CR1_STOP)) {
-        I2Cx->CR1 |= I2C_CR1_STOP; // send stop
-    }
-    I2Cx->CR2 &= ~(I2C_CR2_ITBUFEN | I2C_CR2_ITEVTEN | I2C_CR2_ITERREN); // disable interrupts
+    // disable interrupts
+    HAL_NVIC_DisableIRQ(I2C1_ER_IRQn);
+    HAL_NVIC_DisableIRQ(I2C1_EV_IRQn);
+    // null vars
     currentI2CXAction = NULL;
     currentI2CUnit = NULL;
 }
@@ -360,10 +329,15 @@ void I2C_Internal_GetClockRate( UINT32 rateKhz, UINT8& clockRate, UINT8& clockRa
     NATIVE_PROFILE_HAL_PROCESSOR_I2C();
     if (rateKhz > 400) rateKhz = 400; // upper limit
     UINT32 ccr;
-    if (rateKhz <= 100) { // slow clock
+    if (rateKhz <= 100) 
+    { 
+        // slow clock
         ccr = (SYSTEM_APB1_CLOCK_HZ / 1000 / 2 - 1) / rateKhz + 1; // round up
         if (ccr > 0xFFF) ccr = 0xFFF; // max divider
-    } else { // fast clock
+    } 
+    else 
+    { 
+        // fast clock
         ccr = (SYSTEM_APB1_CLOCK_HZ / 1000 / 3 - 1) / rateKhz + 1; // round up
         ccr |= 0x8000; // set fast mode (duty cycle 1:2)
     }
@@ -375,4 +349,60 @@ void I2C_Internal_GetPins(GPIO_PIN& scl, GPIO_PIN& sda)
 {
     scl = I2C_SCL_PIN;
     sda = I2C_SDA_PIN;
+}
+
+void Start_Master_Transmit(I2C_HAL_XACTION_UNIT* unit, uint16_t I2CAddress)
+{
+    // fill tx buffer
+    uint8_t aTxBuffer[unit->m_bytesToTransfer];
+    uint8_t counter = unit->m_bytesToTransfer;
+    
+    for(int i = 0; i < counter; i++)
+    {
+         // get next data byte from queue
+        aTxBuffer[i] = *(unit->m_dataQueue.Pop());
+    }
+        
+    while(HAL_I2C_Master_Transmit_IT(&I2cHandle, (uint16_t)I2CAddress, (uint8_t*)aTxBuffer, unit->m_bytesToTransfer) != HAL_OK)
+    {
+        /* When Acknowledge failure occurs (Slave don't acknowledge it's address)
+            Master restarts communication */
+        if (HAL_I2C_GetError(&I2cHandle) != HAL_I2C_ERROR_AF)
+        {
+            // couldn't start the transmission because of a timeout error 
+            // nothing else to do because the caller will handle the timeout error
+            
+            // done here
+            return;
+        }
+    }
+    
+    // transmission started correctly
+    // adjust counters
+    unit->m_bytesTransferred = unit->m_bytesToTransfer;
+    unit->m_bytesToTransfer = 0;
+}
+
+void Start_Master_Receive(I2C_HAL_XACTION_UNIT* unit, uint16_t I2CAddress)
+{
+    // allocate rx buffer
+    rxBuffer = (uint8_t *) malloc(unit->m_bytesToTransfer); 
+
+    while(HAL_I2C_Master_Receive_IT(&I2cHandle, (uint16_t)I2CAddress, (uint8_t *)rxBuffer, unit->m_bytesToTransfer) != HAL_OK)
+    {
+        /* When Acknowledge failure occurs (Slave don't acknowledge it's address)
+            Master restarts communication */
+        if (HAL_I2C_GetError(&I2cHandle) != HAL_I2C_ERROR_AF)
+        {
+            // couldn't start the receive because of a timeout error 
+            // nothing else to do because the caller will handle the timeout error
+
+            // free rx buffer
+            free(rxBuffer);
+            rxBuffer = NULL;
+
+            // done here
+            return;
+        }   
+    }   
 }
